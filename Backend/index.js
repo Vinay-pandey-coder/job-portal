f5b225516a9387ed0e4c1d0ae5d35cb1f75fc147
vinay pandey <abhi837688@gmail.com>
2025-12-21 05:39:57 +0530

    application api test working

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/comapny.route.js";
 import jobRoute from './routes/job.route.js'
+import applicationRoute from './routes/applications.route.js'
 dotenv.config({});
 const app = express();
 
@@ -33,6 +34,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
