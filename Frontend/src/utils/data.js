f510ceb235219e2aade9117ee5ab605a882c7ba2
vinay pandey <abhi837688@gmail.com>
2025-12-30 08:32:53 +0530

    project complete

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,6 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/users"
-export const JOB_API_ENDPOINT = "http://localhost:3000/api/job"
-export const APPLICATION_API_ENDPOINT = "http://localhost:3000/api/application"
-export const COMPANY_API_ENDPOINT = "http://localhost:3000/api/company"
+const BASE_URL = "https://job-portal-uora.onrender.com"; 
+
+export const USER_API_ENDPOINT = `${BASE_URL}/api/users`;
+export const JOB_API_ENDPOINT = `${BASE_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}/api/company`;
